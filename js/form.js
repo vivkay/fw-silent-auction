5725de174c5d6a20debba7bff1a39dd5c9a48e69
Vivienne Kay <vivkay8@gmail.com>
2016-02-18 18:52:47 -0500

    feat(*): makes form magic happen

---
+++ js/form.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.js-item').click(function(e){
+    e.preventDefault();
+    console.log($(this).attr('id'));
+    $('.js-input-number').val($(this).attr('id'));
+    $('html, body').animate({
+        scrollTop: $('#form-top').offset().top
+      }, 750);
+  });
+});
+
