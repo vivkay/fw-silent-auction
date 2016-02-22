1565c3fbf99b7749181c9fbb6ab2a88fa4384235
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2016-02-22 00:05:33 -0500

    refactor(html): change favicon source

--- _site/js/form.js
+++ _site/js/form.js
@@ -1,7 +1,7 @@
 $(function(){
-  $('.js-item').click(function(e){
+  $('.js-item').click(function(){
     // e.preventDefault();
-    console.log($(this).attr('id'));
+    // console.log($(this).attr('id'));
     $('.js-input-number').val($(this).attr('id'));
     $('html, body').animate({
         scrollTop: $('#form-top').offset().top
